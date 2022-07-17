1e19f4f726c9d07d1ed1d08599ef2247a3f1dcf4
mracoon <nataliyakomarova777@gmail.com>
2022-07-17 17:44:44 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const bracketsConfigObj = Object.fromEntries(bracketsConfig)
+  const strArr = str.split('')
+  if (str.length % 2 != 0) {
+    return false
+  } else {
+    let resArr = [strArr[0]]
+    for (let i = 1; i < strArr.length; i++) {
+      if (strArr[i] !== bracketsConfigObj[resArr[resArr.length - 1]]) {
+        resArr.push(strArr[i])
+      } else { resArr.pop() }
+    }
+    return resArr.length === 0
+  }
 }
+
